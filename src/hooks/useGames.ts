02fac8f1efc522d7533abaa5c3a9fbb3bba3fe1b
Cody <ccitrano@o-engr.com>
2024-07-24 09:39:17 -0500

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -21,24 +21,29 @@ export interface Game {
     results: Game[];
   }
 
-const useGames = () => {
-    
-        const [games, setGames] = useState<Game[]>([]);
-        const [error, setError] = useState("");
+const useGames = () => {    
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
       
-        useEffect(() => {
-            const controller = new AbortController();    
+  useEffect(() => {
+    const controller = new AbortController();    
 
-          apiClient
-            .get<FetchGamesResponse>("/games", {signal: controller.signal})
-            .then((res) => setGames(res.data.results))
-            .catch((err) => {
-                if(err instanceof CanceledError) return;
-                setError(err.message)
-            });
-            return () => controller.abort();
-        } , []);
-        return {games , error}
-    }
+    setLoading(true);
+    apiClient
+    .get<FetchGamesResponse>("/games", {signal: controller.signal})
+    .then((res) =>{ 
+      setGames(res.data.results);
+      setLoading(false);
+    })
+    .catch((err) => {
+      if(err instanceof CanceledError) return;
+      setError(err.message)
+      setLoading(false);
+    });
+    return () => controller.abort();
+  } , []);
+  return {games , error, isLoading}
+}
 
 export default useGames;
